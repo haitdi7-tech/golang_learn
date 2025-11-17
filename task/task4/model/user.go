148801eed103446cfad29b4beb165b8517342332
haitdi7-tech <haitdi7@gmail.com>
2025-11-17 13:26:56 +0800

    Update user.go

--- task/task4/model/user.go
+++ task/task4/model/user.go
@@ -1,1 +1,13 @@
+package model
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	UserName string    `gorm:"unique;not null" json:"username"`
+	Password string    `gorm:"not null" json:"-"`
+	Email    string    `gorm:"unique;not null" json:"email"`
+	Posts    []Post    `gorm:"foreignKey:UserId" json:"posts,omitempty"`
+	Comments []Comment `gorm:"foreignKey:UserId" json:"comments,omitempty"`
+}
 
